8be8daadc96a551d9ee554ce856087a2e0cb7eca
me.luc <lukasalmeida372@gmail.com>
2023-05-15 21:26:15 -0300

    feat: rooms info router

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { bookingRoom, changeBooking, listBooking } from '@/controllers';
+import { bookingRoom, changeBooking, listBooking, listRoomsInfo } from '@/controllers';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('', listBooking).post('', bookingRoom).put('/:bookingId', changeBooking);
+bookingRouter
+  .all('/*', authenticateToken)
+  .get('', listBooking)
+  .post('', bookingRoom)
+  .put('/:bookingId', changeBooking)
+  .get('info/rooms', listRoomsInfo);
 
 export { bookingRouter };
