4915203a38a9dea53ccdfd9b7cf5e76a94dc422e
me.luc <lukasalmeida372@gmail.com>
2023-05-23 22:19:58 -0300

    feat: add get activities by day repository

---
+++ src/repositories/activities-repository/index.ts
@@ -0,0 +1,15 @@
+import { Dayjs } from 'dayjs';
+import { prisma } from '@/config';
+
+export async function getActivitiesByDay(targetDate: Dayjs) {
+  const activities = await prisma.activity.findMany({
+    where: {
+      startsAt: {
+        gte: targetDate.startOf('day').toDate(),
+        lt: targetDate.endOf('day').toDate(),
+      },
+    },
+  });
+
+  return activities;
+}
