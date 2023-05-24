00ddf2e926a2476f1c07d321084f54d01b0c0508
me.luc <lukasalmeida372@gmail.com>
2023-05-24 20:59:46 -0300

    feat: update table name

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,5 +1,7 @@
 import { Dayjs } from 'dayjs';
 import { prisma } from '@/config';
+import { notFoundError } from '@/errors';
+import ticketService from '@/services/tickets-service';
 
 export async function getActivitiesByDay(targetDate: Dayjs) {
   const activities = await prisma.activity.findMany({
@@ -24,4 +26,44 @@ export async function getDayActivities() {
 
 export async function getActivities() {
   return await prisma.activity.findMany();
+}
+
+export async function subscribeToActivity(userId: number, activityId: number) {
+  const activity = await prisma.activity.findUnique({
+    where: {
+      id: activityId,
+    },
+  });
+
+  if (!activity) throw notFoundError();
+
+  if (userTicket.status !== 'PAID') throw Error('Ticket not paid');
+
+  const ticketType = await prisma.ticketType.findUnique({
+    where: {
+      id: userTicket.ticketTypeId,
+    },
+  });
+
+  if (!ticketType) throw Error('Ticket type not found');
+
+  if (!ticketType.includesHotel || ticketType.isRemote) throw Error('Ticket type not allowed');
+
+  const activitySubscription = await prisma.activitySubscription.findUnique({
+    where: {
+      userId_activityId: {
+        userId,
+        activityId,
+      },
+    },
+  });
+
+  if (activitySubscription) throw Error('User already subscribed to activity');
+
+  return await prisma.activitySubscription.create({
+    data: {
+      userId,
+      activityId,
+    },
+  });
 }
