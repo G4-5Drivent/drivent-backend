63631a4f25c08639c634b84a5d38a131b74046c6
me.luc <lukasalmeida372@gmail.com>
2023-05-24 16:10:06 -0300

    feat: update get activities by day

--- src/services/activity-service/index.ts
+++ src/services/activity-service/index.ts
@@ -1,13 +1,25 @@
 import dayjs from 'dayjs';
+import ticketService from '../tickets-service';
 import { badRequestError } from '@/errors/bad-request-error';
 import * as activityRepository from '@/repositories/activities-repository';
+import { notFoundError } from '@/errors';
+import { forBiddenError } from '@/errors/forbidden-error';
 
-export async function getActivitiesByDay(targetDate: string) {
-  // VERIFICAR SE USUARIO TEM TICKET PAGO
-  // VERIFICAR SE USUARIO TEM TICKET PRESENCIAL
-  const date = dayjs(targetDate);
+export async function getActivitiesByDay(userId: number, date: string) {
+  const userTicket = await ticketService.getTicketByUserId(userId);
+
+  if (!userTicket) throw notFoundError();
+
+  if (userTicket.status !== 'RESERVED') throw forBiddenError();
+
+  const ticketTypes = await ticketService.getTicketType();
+  const ticketType = ticketTypes.find((type) => type.id === userTicket.ticketTypeId);
+  if (!ticketType) throw Error('Ticket type not found');
+  if (!ticketType.includesHotel || ticketType.isRemote) throw forBiddenError();
+
+  const targetDate = dayjs(date);
 
   if (!date.isValid()) throw badRequestError();
 
-  return activityRepository.getActivitiesByDay(date);
+  return activityRepository.getActivitiesByDay(targetDate);
 }
