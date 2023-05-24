7eb6039be17ca07f2273bbda2215c3dec177a7dd
me.luc <lukasalmeida372@gmail.com>
2023-05-24 16:13:40 -0300

    feat: add activity service to index

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,6 @@ export * from './users-service';
 export * from './authentication-service';
 export * from './events-service';
 export * from './enrollments-service';
+// eslint-disable-next-line import/export
+export * from './tickets-service';
+export * from './activity-service';
