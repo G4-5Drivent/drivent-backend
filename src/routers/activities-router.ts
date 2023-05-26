f598032697c4b113ecf49c1452e2a52cf051dacf
me.luc <lukasalmeida372@gmail.com>
2023-05-25 21:02:22 -0300

    feat: add unsubscribe router

--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getActivitiesByDay, getDayActivities, subscribeToActivity } from '@/controllers';
+import { getActivitiesByDay, getDayActivities, subscribeToActivity, unsubscribeToActivity } from '@/controllers';
 
 const activitiesRouter = Router();
 activitiesRouter
   .use('/*', authenticateToken)
   .post('', subscribeToActivity)
   .get('/date', getActivitiesByDay)
-  .get('/days', getDayActivities);
+  .get('/days', getDayActivities)
+  .delete('', unsubscribeToActivity);
 
 export { activitiesRouter };
